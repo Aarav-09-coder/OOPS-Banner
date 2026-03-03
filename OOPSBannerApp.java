e09f4541443728e82553d5264fc937a2c934a264
Aarav-09-coder <ag5857@srmist.edu.in>
2026-03-03 09:02:05 +0530

    Update and rename OOPSCONSOLE.java to  OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,4 @@
-public class OOPSCONSOLE{
+public class  OOPSBannerApp{
 
 	public static void main(String [] args){
 		System.out.println("   ***   " + " " + "   ***   " + " " + "*****  " + " " + "*****");
