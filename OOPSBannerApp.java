dcd1405221a54bdfa8c77b234fb83e3188f8937a
Aarav-09-coder <ag5857@srmist.edu.in>
2026-03-03 09:01:37 +0530

    Update and rename OOPSAPP.java to  OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,4 @@
-public class OOPSAPP {
+public class OOPSBannerApp {
    public static void main(String[] args) {
         System.out.println(String.join(" ","   ***   " , "   ***   " , "*****  " , "*****"));
 		System.out.println(String.join(" "," **   ** " , " **   ** " , "*     *" , "*    "));
